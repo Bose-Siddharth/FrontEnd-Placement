c1eac7969b267c2713d172a10ef70eedc548d3ad
Arunava Kundu <drarunkundu22@gmail.com>
2022-12-28 16:30:45 +0530

    Update Index.tsx

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'daus-one.s3.ap-south-1.amazonaws.comcv.jpg',
+        port: '',
+        pathname: '/uem-placement/**',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
